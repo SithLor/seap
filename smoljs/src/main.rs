73014c09be184c4b9b5de34c87d328249f6bd605
SithLor <84148006+SithLor@users.noreply.github.com>
2024-02-01 21:10:07 +0000

    d

--- smoljs/src/main.rs
+++ smoljs/src/main.rs
@@ -1,17 +1,29 @@
-use std::task::Context;
+use rusty_jsc::{JSContext, JSValue};
+use rusty_jsc_macros::callback;
+use base64::prelude::*;
 
+#[callback]
+fn greet(
+    ctx: JSContext,
+    function: JSObject,
+    this: JSObject,
+    args: &[JSValue],
+) -> Result<JSValue, JSValue> {
+    Ok(JSValue::string(&ctx, format!("Hello, {}", args[0].to_string(&ctx).unwrap())))
+}
+//https://github.com/wasmerio/rusty_jsc/blob/main/examples/hello.rs
+fn main() {
+    let mut context: JSContext = JSContext::default();
+    let callback: JSValue = JSValue::callback(&context, Some(greet));
+    let global: rusty_jsc::JSObject = context.get_global_object();
+    global.set_property(&context, "greet", callback).unwrap();
 
-
-
-
-fn main(){
-    //bootstarp 
-    let mut context: rusty_jsc::JSContext = rusty_jsc::JSContext::default();
-    let value = context.evaluate_script("'hello, world'", 1);
-    if let Ok(value) = value {
-        println!("{}", value.to_js_string(&context).unwrap());
-    } else {
-        let ex = value.unwrap_err().to_js_string(&context).unwrap();
-        println!("Uncaught: {}", ex);
+    match context.evaluate_script("greet('Tom')", 1) {
+        Ok(value) => {
+            println!("{}", value.to_string(&context).unwrap());
+        }
+        Err(e) => {
+            println!("Uncaught: {}", e.to_string(&context).unwrap())
+        }
     }
 }
