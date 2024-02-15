4aafda09f90c59385a01dbb4d7b7d74097362222
SithLor <84148006+SithLor@users.noreply.github.com>
2024-02-15 21:28:46 +0000

    e

--- java/com/Main.java
+++ java/com/Main.java
@@ -4,7 +4,6 @@ package com;
 
 public class Main {
     public static void main(String[] args) {
-        String e = java.util.Map
         System.out.println("Hello, World!"); 
     }
 }
--- java/com/sutil.java
+++ java/com/sutil.java
@@ -1,12 +1,13 @@
 package com;
 //work on Base64 implementation
 
-class StringLengthLessThanOne extends Exception  {
-    public StringLengthLessThanOne(){
-        super("StringLengthLessThanOne");
-    }
-}
+
 class sutil {
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
     }
