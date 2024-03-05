881c9068fa5e6df156f9b67d5d41c5d02ab86c58
SithLor <84148006+SithLor@users.noreply.github.com>
2024-03-05 21:42:32 +0000

    kjkllkjl

--- exs/client/main.java
+++ exs/client/main.java
@@ -11,40 +11,56 @@ import java.io.PrintWriter;
 import java.net.Socket;
 //Screen Shot Support:NO
 class Funcs {
-    public static String GetIpAddr() {
-        final String Data = "0.0.0.0";
-        String API_URL = "https://api.ipify.org";
-        try {
-            URL url = new URL(API_URL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line;
-            StringBuilder result = new StringBuilder();
-            while ((line = rd.readLine()) != null) {
-                result.append(line);
-            }
-            rd.close();
-            return result.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "0.0.0.0";
-        }
-
+    public static int bytesizeofString(String e){
+        int data = e.length() * 2;
+        return data;
     }
-    public static int sizeof(Object a){
+    public static double sizeof_primitive(Object a){
         //get the byte size of the object
-        String type = Object.class.getTypeName();
-        System.out.println(types);
+        String type = a.getClass().getTypeName();
+        System.out.println(type);
+
+        double size_byte = 0;
+        switch (type) {
+            case "java.lang.Byte":
+                size_byte = 1;
+                break;
+            case "java.lang.Short":
+                size_byte = 2;
+                break;
+            case "java.lang.Integer":
+                size_byte = 4;
+                break;
+            case "java.lang.Long":
+                size_byte = 8;
+                break;
+            case "java.lang.Float":
+                size_byte = 4;
+                break;
+            case "java.lang.Double":
+                size_byte = 8;
+                break;
+            case "java.lang.Boolean":
+                size_byte= 0.125;
+                break;
+            default:
+                break;
+        }
+        return size_byte;
     }
     
 }
 public class main {
     
     public static void main(String[] args) {
-        short ARG_1 = 1;
-        Funcs.sizeof(args)
-        
+        byte a_1 = 0;
+        short a_2 = 0;
+        int a_3 = 0;
+        long a_4 = 0;
+        float a_5 = 0;
+        double a_6 = 0.0;
+        boolean a_7 = false;
+        System.out.println(Funcs.sizeof_primitive(a_1));        
     }
     
     private static void log(String d) {
