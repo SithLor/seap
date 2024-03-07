d27b69f2831dcca45cda8082ed279dfcb3f79d54
SithLor <84148006+SithLor@users.noreply.github.com>
2024-03-07 18:37:44 +0000

    Fix IsDebuggerPresent function and add Windows target for Rust

--- install.rust.sh
+++ install.rust.sh
@@ -9,4 +9,9 @@ cargo install wasm-pack
 wget https://github.com/sharkdp/hyperfine/releases/download/v1.16.1/hyperfine_1.16.1_amd64.deb
 sudo dpkg -i hyperfine_1.16.1_amd64.deb
 curl -fsSL https://bun.sh/install | bash
+
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 pip install -r requirements.txt
+rustup target add x86_64-pc-windows-gnu
+sudo apt-get install gcc-mingw-w64-x86-64 -y
