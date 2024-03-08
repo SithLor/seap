639fbd7a3333ea564ab1ceb03b198cd54b3e4a7b
SithLor <84148006+SithLor@users.noreply.github.com>
2024-03-08 18:08:18 +0000

    Add NTAPI STUBS

--- fawn-app/src/main.rs
+++ fawn-app/src/main.rs
@@ -7,38 +7,9 @@
 //https://www.vergiliusproject.com/kernels/x64/Windows%2010%20%7C%202016/2110%2021H2%20(November%202021%20Update)/_OBJECT_ATTRIBUTES
 //https://www.vergiliusproject.com/kernels/x64/Windows%2011
 //https://www.geoffchappell.com/studies/windows/km/ntoskrnl/inc/api/pebteb/peb/index.htm
-
-
-use std::arch::asm;
-
-use std::os::raw::{c_void, c_uchar};
-
-#[repr(C)]
-struct RTL_USER_PROCESS_PARAMETERS {
-    // Define the fields of the RTL_USER_PROCESS_PARAMETERS structure here
-}
-
-#[repr(C)]
-struct PTEB {
-    _reserved: [u8; 0x02], // Reserve space for the fields before BeingDebugged
-    BeingDebugged: c_uchar,
-    _reserved2: [u8; 0x1D], // Reserve space for the fields between BeingDebugged and ProcessParameters
-    ProcessParameters: *const RTL_USER_PROCESS_PARAMETERS,
-    // Define the rest of the fields of the TEB here
-}
+//http://undocumented.ntinternals.net/index.html?page=UserMode%2FStructures%2FRTL_DRIVE_LETTER_CURDIR.html
+mod windows;
 
 fn main() {
-    let offset: i32 = 0x60;
-    let teb: *const PTEB;
-    unsafe {
-        asm!(
-            "mov {0}, gs:[{1}]",
-            out(reg) teb,
-            in(reg) offset,
-        );
-        // Now `teb` is a pointer to the TEB
-        // You can access the BeingDebugged field like this:
-        let being_debugged = (*teb).BeingDebugged;
-        println!("BeingDebugged: {}", being_debugged);
-    }
+    print!("Hello, world!")
 }
