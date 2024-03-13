3a8b29a3e41b5b17adf3837a0a212fd251a76d1a
SithLor <84148006+SithLor@users.noreply.github.com>
2024-03-13 18:48:27 +0000

    m

--- fawn-app/src/main.rs
+++ fawn-app/src/main.rs
@@ -1,14 +1,4 @@
 
-fn test(){
-
-    use fawn_lib::custom::nt::peb::ProcessParameters;
-    let e: *mut fawn_lib::custom::nt::peb::RTL_USER_PROCESS_PARAMETERS = ProcessParameters();
-    println!("{:?}", e);
-
-
-
-
-}
 fn main() {
-    test()
+
 }
