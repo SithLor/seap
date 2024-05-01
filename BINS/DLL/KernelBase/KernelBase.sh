f45c85b1c5b01a03b5e893cf524e644a248287a7
SithLor <84148006+SithLor@users.noreply.github.com>
2024-05-01 17:18:58 +0000

    e

--- BINS/DLL/KernelBase/KernelBase.sh
+++
@@ -1,8 +0,0 @@
-objdump -D  ./KernelBase.dll  > KernelBase.objdump.att.asm
-objdump -x  ./KernelBase.dll  > KernelBase.objdump.header
-objdump -f  ./KernelBase.dll  > KernelBase.objdump.fileheader
-objdump -p  ./KernelBase.dll  > KernelBase.objdump.privatefileheader
-objdump -t  ./KernelBase.dll  > KernelBase.objdump.symboltable
-objdump -T  ./KernelBase.dll  > KernelBase.objdump.dynamicsymboltable
-objdump -R  ./KernelBase.dll  > KernelBase.objdump.dynamicrelocationtable
-objdump -r  ./KernelBase.dll  > KernelBase.objdump.relocationtable
