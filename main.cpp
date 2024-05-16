e7823aa673533fc122e454d41e3932baa5a1b369
SithLor <84148006+SithLor@users.noreply.github.com>
2024-05-16 15:59:09 +0000

    chore: Refactor code for better organization and readability

---
+++ main.cpp
@@ -0,0 +1,7 @@
+int main(){
+    //array of 1000 integers
+    int arr[1000];
+    for(int i = 0; i < 1000; i++){
+        arr[i] = i;
+    }
+}
--- silly/src/main.rs
+++ silly/src/main.rs
@@ -1,10 +1,28 @@
+trait Plugin {
+    fn run(&self);
+    fn exit(&self);
+}
 
+struct MyPlugin;
 
-//https://godbolt.org/
+impl Plugin for MyPlugin {
+    fn run(&self) {
+        println!("MyPlugin run");
+    }
+    fn exit(&self) {
+        println!("MyPlugin exit");
+    }
+}
 
-
+impl Drop for MyPlugin {
+    fn drop(&mut self) {
+        self.exit();
+    }
+}
 
 
 fn main() {
-    
+    let my_plugin: MyPlugin = MyPlugin;
+    my_plugin.run();
+    // my_plugin will be dropped here, and the exit method will be called
 }
