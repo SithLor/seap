d0dfdf350daf3f80dda9d2d329623c7b23b90204
SithLor <84148006+SithLor@users.noreply.github.com>
2024-06-13 16:15:34 +0000

    almost done vfs

--- alg_core/src/lib.rs
+++ alg_core/src/lib.rs
@@ -4,11 +4,106 @@
 
 
 pub mod uri;
+
+pub mod vfs {
+    //light weight file system for browser for load scripts
+
+    
+    pub struct File {
+        pub parent_folder: String,
+        pub name: String,
+        pub content: Vec<u8>,
+    }
+    pub struct term_vfs {
+        pub files: Vec<File>,
+        pub folders: Vec<String>,
+        pub current_dir: String,
+    }
+    impl term_vfs {
+        pub fn new() -> Self {
+            term_vfs {
+                files: vec![],
+                folders: vec![],
+                current_dir: "/".to_string(),
+            }
+        }
+        pub fn change_dir(&mut self, dir: &str) {
+            //check if dir exists else print error
+            match self.folders.iter().find(|&x| x == dir) {
+                Some(_) => {
+                    self.current_dir = dir.to_string();
+                }
+                None => {
+                    println!("Error: Directory does not exist");
+                }
+            }
+        }
+        pub fn create_dir(&mut self, dir: &str) {
+            //check if dir exists else print error
+            match self.folders.iter().find(|&x| x == dir) {
+                Some(_) => {
+                    println!("Error: Directory already exists");
+                }
+                None => {
+                    self.folders.push(dir.to_string());
+                }
+            }
+        }
+        pub fn create_file(&mut self, name:String,data:u8){
+            //check if file exists else print error
+            match self.files.iter().find(|&x| x.name == name) {
+                Some(_) => {
+                    println!("Error: File already exists");
+                }
+                None => {
+                    self.files.push(File {
+                        parent_folder: self.current_dir.clone(),
+                        name: name,
+                        content: vec![data],
+                    });
+                }
+            }
+        }
+    }
+
+    
+}
 #[cfg(test)]
 mod tests {
+    use super::vfs::term_vfs;
+    //vfs test for browser that use rust as a scripting language
+    #[test]
+    fn test_vfs_new() {
+        let vfs = term_vfs::new();
+        assert_eq!(vfs.files.len(), 0);
+        assert_eq!(vfs.folders.len(), 0);
+        assert_eq!(vfs.current_dir, "/");
+    }
+
+    #[test]
+    fn test_vfs_change_dir() {
+        let mut vfs = term_vfs::new();
+        vfs.folders.push("/home".to_string());
+        vfs.change_dir("/home");
+        assert_eq!(vfs.current_dir, "/home");
+    }
+    #[test]
+    fn test_vfs_create_dir() {
+        let mut vfs = term_vfs::new();
+        vfs.create_dir("/home");
+        assert!(vfs.folders.contains(&"/home".to_string()));
+    }
+
+    #[test]
+    fn test_vfs_create_file() {
+        let mut vfs = term_vfs::new();
+        vfs.create_file("file1".to_string(), 1);
+        assert!(vfs.files.iter().any(|file| file.name == "file1"));
+    }
+
+
     use std::net::Ipv6Addr;
     use std::str::FromStr;
-
     use crate::uri::{Host, URI,UriGetterSetter};
     #[test]
     fn test_ipv6_addr() {
@@ -30,36 +125,5 @@ mod tests {
 
         let expected = "http://user:password@example.com:8080/path?query#fragment";
         assert_eq!(uri.to_string(), expected);
-    }
-    #[test]
-    fn test_uri_urigetter_setter() {
-        let mut uri = URI::from_str("http://example.com/path").unwrap();
-        assert_eq!(uri.get_scheme(), "http");
-        uri.set_scheme("https".to_string());
-        assert_eq!(uri.get_scheme(), "https");
-
-        assert_eq!(uri.get_user_info(), None);
-        uri.set_user_info(Some("user:password".to_string()));
-        assert_eq!(uri.get_user_info(), Some("user:password".to_string()));
-        
-        assert_eq!(uri.get_host(), Host::RegisteredName("example.com".to_string()));
-        uri.set_host(Host::IP("127.0.0.1".parse().unwrap()));
-        assert_eq!(uri.get_host(), Host::IP("127.0.0.1".parse().unwrap()));
-
-        assert_eq!(uri.get_port(), None);
-        uri.set_port(Some(8080));
-        assert_eq!(uri.get_port(), Some(8080));
-
-        assert_eq!(uri.get_path(), "/path");
-        uri.set_path("/new_path".to_string());
-        assert_eq!(uri.get_path(), "/new_path");
-
-        assert_eq!(uri.get_query(), None);
-        uri.set_query(Some("query".to_string()) );
-        assert_eq!(uri.get_query(), Some("query".to_string()));
-
-        assert_eq!(uri.get_fragment(), None);
-        uri.set_fragment(Some("fragment".to_string()));
-        assert_eq!(uri.get_fragment(), Some("fragment".to_string()));
     }
 }
